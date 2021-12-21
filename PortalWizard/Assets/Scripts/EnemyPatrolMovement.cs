f176a4574a35c1ea52e17566a6b8877b267da6da
Arkadiusz-T <69872743+Arkadiusz-T@users.noreply.github.com>
2021-12-21 12:21:02 +0100

    working on enemy fliping

--- PortalWizard/Assets/Scripts/EnemyPatrolMovement.cs
+++ PortalWizard/Assets/Scripts/EnemyPatrolMovement.cs
@@ -6,15 +6,16 @@ public class EnemyPatrolMovement : MonoBehaviour
 {
 
     public float movementSpeed;
+    public static RaycastHit2D rc;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rc = Physics2D.Raycast(transform.position, Vector2.left, 0.2f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(-1, 0, 0) * Time.deltaTime * movementSpeed;
+            transform.position += new Vector3(-1, 0, 0) * Time.deltaTime * movementSpeed;
     }
 }
